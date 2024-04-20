ec57f6e61c16ee12b7807059f27a762058e5bb24
Narestur <artemgutsal@gmail.com>
2024-04-20 16:37:28 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,37 @@
+$(function () {
+
+    $('.product_slider').slick({
+        slidesToShow: 5,
+        slidesToScroll: 5,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3
+                }
+            },
+            {
+                breakpoint: 820,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+
+    });
+
+
+});
+function toggle(el)
+{
+	el.style.display = (el.style.display == 'none') ? 'block' : 'none';
+}
